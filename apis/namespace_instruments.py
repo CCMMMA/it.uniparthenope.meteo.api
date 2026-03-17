38e91c01a755e97db9888fbd92ba52011a8ac4d9
DarioCaramiello <dario.caramiello001@studenti.uniparthenope.it>
2026-03-17 10:09:41 +0100

    Edit: Get requests to signalk api

--- apis/namespace_instruments.py
+++ apis/namespace_instruments.py
@@ -1,14 +1,67 @@
-import json
+#################################################
+#   
+#   Università Degli Studi di Napoli Parthenope 
+#
+#
+# Authors: 
+#    Prof. Raffaele Montella
+#    Dario Caramiello   
+#
+#################################################
+
 from flask_restx import Namespace, Resource
 from flask import jsonify, Response, make_response, request
+
+from core.Logger import logger
 from core.MeteoServices import MeteoServices
-import app
 from core.MemcachedMethodHandlers import get_resource, set_resource
 
+import json
+import app
 
 api = Namespace('instruments', description='Instruments API')
 
+@api.route('')
+class Instruments(Resource):
+    @api.doc()
+    def get(self):
+        """Returns the avaliable instruments.
+        :example: /products
+        :returns:  json -- the return json.
+        """
 
+        ms = MeteoServices(app.application.config)
+        res = ms.getInstruments()
+
+        '''
+        print("\n\n")
+        logger.info(f"res : {res}")
+        print("\n\n")
+        '''
+
+        for elem in res.items():
+            print("\n\n")
+            logger.info(f"elem : {elem}")
+            print("\n\n")
+
+        return jsonify(res)
+
+
+@api.route('/<string:identification>')
+class InstrumentsContext(Resource):
+    @api.doc()
+    def get(self, identification):
+
+        ms = MeteoServices(app.application.config)
+        res = ms.getInstruments()
+
+        for ws_id, ws_data in res.items():
+            if ws_id == identification:
+                return ws_data
+        
+        return "Identification not found!"
+
+'''
 # TESTED AND WORKING -- USE MEMCACHE AND DISKCACHE 
 @api.route('')
 class Instruments(Resource):
@@ -44,6 +97,6 @@ class Instruments(Resource):
                 #return jsonify(res)
                
         return res
-
+'''
 
         
